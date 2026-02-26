464143c7e6b1e4eb12e017179dc79da2ff31f7d8
DevAlysonh <dev.alysonh@gmail.com>
2026-02-26 19:52:31 -0300

    wip: melhora tratamento de erros

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -29,16 +29,4 @@ return Application::configure(basePath: dirname(__DIR__))
                 'error' => 'invalid_zipcode',
             ], Response::HTTP_UNPROCESSABLE_ENTITY);
         });
-
-        $exceptions->render(function (Throwable $e, $request) {
-            new LaravelLogger()->error('Um erro interno inesperado ocorreu', [
-                'error' => $e->getMessage(),
-                'exception' => get_class($e),
-            ]);
-
-            return response()->json([
-                'message' => 'Um erro interno inesperado ocorreu. Por favor, tente novamente mais tarde.',
-                'error' => 'internal_server_error',
-            ], Response::HTTP_INTERNAL_SERVER_ERROR);
-        });
     })->create();
