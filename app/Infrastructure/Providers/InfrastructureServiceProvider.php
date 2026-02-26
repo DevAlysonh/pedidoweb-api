89d9e110b2ed4d644969c446059cf1bca7def802
DevAlysonh <dev.alysonh@gmail.com>
2026-02-26 18:13:00 -0300

    wip: move o bind do DI para o provider de infraestrutura

--- app/Infrastructure/Providers/InfrastructureServiceProvider.php
+++ app/Infrastructure/Providers/InfrastructureServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Infrastructure\Providers;
 
 use App\Domain\Shared\Interfaces\IdGenerator;
+use App\Infrastructure\Services\CepService;
+use App\Infrastructure\Services\ViaCepService;
 use App\Infrastructure\Shared\UuidGenerator;
 use Illuminate\Support\ServiceProvider;
 
@@ -10,10 +12,8 @@ final class InfrastructureServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app->bind(
-            IdGenerator::class,
-            UuidGenerator::class
-        );
+        $this->app->bind(IdGenerator::class, UuidGenerator::class);
+        $this->app->bind(CepService::class, ViaCepService::class);
     }
 
     public function boot(): void
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,15 +2,13 @@
 
 namespace App\Providers;
 
-use App\Infrastructure\Services\CepService;
-use App\Infrastructure\Services\ViaCepService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->app->bind(CepService::class, ViaCepService::class);
+        //
     }
 
     public function boot(): void
